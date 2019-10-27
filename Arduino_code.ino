c491daee75921be71c3fd753803aab6379df738f
Panellet <32815065+Panellet@users.noreply.github.com>
2019-10-27 20:37:28 +0100

    Update Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -1,9 +1,9 @@
-//1. Set the Arduino off as Default value when connection starts.
+//1. Defining the communication protocol between Arduino and RoboRemo app
 
-int on = 0; // off
-
+int on = 0; // means that Arduino is off as default.
 char cmd[100];
 int cmdIndex;
+
 
 boolean cmdStartsWith(char *st) {
   for(int i=0; ; i++) {
@@ -15,27 +15,27 @@ boolean cmdStartsWith(char *st) {
 }
 
 
+//2. Defining the relation between buttons in the roboremo-interface.interface and what Arduino recieves. 
+
 void exeCmd() {
   
-  if( cmdStartsWith("on") ) on = 1;
-  if( cmdStartsWith("off") ) on = 0;
+  if( cmdStartsWith("on") ) on = 1; //The command named "on" in the interface changes the variable on to 1.
+  if( cmdStartsWith("off") ) on = 0;//The command named "off" in the interface changes the variable on to 0.
      
 }
 
-
+//3. Initiating the USB OTG Serial communication protocol between Arduino/Genuino and the RoboRemo app.
 
 void setup() {
   
   delay(500); 
 
-  Serial.begin(576000);
-  
-  //analogReference(INTERNAL); // 1.1V on UNO/NANO
+  Serial.begin(115200); //This value corresponds to the speed in bits per second (baud). 
   
   cmdIndex = 0;
 }
 
-
+//4. Defining variables to allow communication between the Arduino/Genuino board and the RoboRemo app.
 
 void loop() {
   
@@ -54,9 +54,11 @@ void loop() {
     
   }
   
+//5.   
+  
   if(on) {
-    int val = analogRead(A0);
-    String st = (String)"v " + ((val * (5.0 / 1023.0))-0.6529)*1.68 + "\n";
+    int val = analogRead(A0); //Sets the A0 pin in Arduino/Genuino Board as the input pin.
+    String st = (String)"v " + ((val * (5.0 / 1023.0))-0.6529)*1.68 + "\n"; //
     Serial.print(st);
     delay(1);
   }
